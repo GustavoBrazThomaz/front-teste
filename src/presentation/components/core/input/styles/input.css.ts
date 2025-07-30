003b57c03f152c4abe4ed305cee98a56edc6a174
Gustavo Braz <86533841+GustavoBrazThomaz@users.noreply.github.com>
2025-07-30 16:53:27 -0300

    feat: add input component

---
+++ src/presentation/components/core/input/styles/input.css.ts
@@ -0,0 +1,25 @@
+import { theme } from "@components/styles/theme/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const inputStyle = style({
+  backgroundColor: theme.colors.primarySurface,
+  color: theme.colors.primaryForeground,
+  padding: "1.3rem",
+  borderRadius: "1rem",
+  border: `1px solid ${theme.colors.secondaryForeground}`,
+
+  selectors: {
+    "&:disabled": {
+      borderColor: theme.colors.disabledForeground,
+      backgroundColor: theme.colors.disabledBackground,
+      cursor: "not-allowed"
+    },
+    "&:focus": {
+      outline: "none",
+      borderColor: theme.colors.foregroundSelectionBackground,
+    },
+    "&:invalid": {
+      borderColor: theme.colors.error,
+    },
+  },
+});
