a7b6970cfbec5894a045192119d4e91a8b0e4e33
Gustavo Braz <86533841+GustavoBrazThomaz@users.noreply.github.com>
2025-08-04 18:44:32 -0300

    feat: add skeleton component

---
+++ src/presentation/components/core/skeleton/styles/skeleton.css.ts
@@ -0,0 +1,16 @@
+import { theme } from "@components/styles/theme/theme.css";
+import { keyframes, style } from "@vanilla-extract/css";
+
+const skeletonPulseKeyframes = keyframes({
+  "0%": { opacity: 1 },
+  "50%": { opacity: 0.5 },
+  "100%": { opacity: 1 },
+});
+
+export const skeletonStyles = style({
+  backgroundColor: theme.colors.skeletonBackground,
+  animationName: skeletonPulseKeyframes,
+  animationDuration: "1.5s",
+  animationTimingFunction: "ease-in-out",
+  animationIterationCount: "infinite",
+});
